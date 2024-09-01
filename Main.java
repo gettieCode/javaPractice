c311621ca1e9fcb53943fd1e4dc9489f147a018c
Gertrude Kristine Agena <saucegettie@gmail.com>
2024-09-01 14:16:46 +0800

    002 | Day 2 practice git commit

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.*; //scanner
 
 public class Main {
     public static void main (String[] args){
-        /* 
+    /* 
         float height = 1.5932f; ///lagi may letter f if wala gamitin ay double
 
         char letter ='a', digit = 0;
@@ -16,7 +16,7 @@ public class Main {
         String str1 = "pass";
         String str2 = "Pass";
         boolean isEqual = str1.equals(str2);
-        System.out.println(isEqual); //true
+        System.out.println(isEqual); //false
 
         //copying
         String str3 = "mama"; 
@@ -30,7 +30,33 @@ public class Main {
         String nameMo = input.nextLine(); //create ng placeholder
         System.out.println(nameMo); //print input
         input.close(); //always close
-*/
 
+        //pascal's triangle using nCr formula
+*/
+        System.out.println(74+36);
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = input.nextInt();
+        System.out.print("input second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println(num1 + " + " + num2 + " = "+ (num1+num2));
+        System.out.println(num1 + " - " + num2 + " = "+ (num1-num2));
+        System.out.println(num1 + " x " + num2 + " = "+ num1*num2);
+        System.out.println(num1 + " / " + num2 + " = "+ num1/num2);
+        System.out.println(num1 + " r " + num2 + " = "+ num1%num2);
+
+        System.out.print("Multiplication table: ");
+        int mulNum = input.nextInt();
+        for (int i = 1; i<=10; i++){
+            System.out.println(mulNum + " x " + i + " = " + mulNum*i);
+        }
+        System.out.println((25.5*3.5-3.5*3.5)/(40.5-4.5));
+        input.close();
     }
 }
